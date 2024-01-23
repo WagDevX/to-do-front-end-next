f4edec1236e1cf8b47c856059d241275350a448a
WagDevX <waguinbk@gmail.com>
2024-01-22 22:38:31 -0300

    git ignore fix

--- app/lib/definitions.ts
+++
@@ -1,9 +0,0 @@
-export type NoteType = {
-  _id: string;
-  title: string;
-  description: string;
-  color: string | undefined;
-  favorited: boolean;
-  createdAt: string;
-  updatedAt: string;
-};
--- app/ui/loader/spinner-loader.tsx
+++
@@ -1,5 +0,0 @@
-import { ClipLoader } from "react-spinners";
-
-export default function SpinnerLoader({ size = 35 }: { size: number }) {
-  return <ClipLoader color="#51646E" size={size} />;
-}
